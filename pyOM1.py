2405d28b1ec05f0ece73a9ceec46433129eb450b
Shriniwas Deshmukh <76507312+Sinudeshmukh@users.noreply.github.com>
2021-04-22 23:44:58 +0530

    Add files via upload

---
+++ pyOM1.py
@@ -0,0 +1,11 @@
+
+import os
+
+print(os.getcwd())
+
+# os.mkdir("test")
+
+if os.path.exists("test"):
+    print("Directory already exists")
+else:
+    os.mkdir("test")
---
+++ pynotepad.py
@@ -0,0 +1,7 @@
+import os
+
+# os.system("notepad.exe")
+prg = os.getcwd() + "\\pyDT2.py"
+print("prg: ", prg)
+
+os.system(prg)
